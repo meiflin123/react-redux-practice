803d3f4ec0260b65bfc60aa569d9cd807834b6cc
Meifang Lin <meiflin123@gmail.com>
2019-01-23 18:07:45 -0800

    create jsx data for ApprovalCard

--- mini-app-components/src/ApprovalCard.js
+++ mini-app-components/src/ApprovalCard.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const ApprovalCard = () => {
-  return <div>Hi ApprovalCard!</div>
-
+  return (
+    <div className="ui card">
+      <div className="content">hi ApprovalCard!</div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui basic green button">Approve</div>
+          <div className="ui basic red button">Reject</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 

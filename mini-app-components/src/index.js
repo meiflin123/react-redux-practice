afafc2d7be5b32827366d8a6ac9603e2635b8031
Meifang Lin <meiflin123@gmail.com>
2019-01-23 18:10:29 -0800

    pass CommentDetail as children into component ApprovalCard

--- mini-app-components/src/index.js
+++ mini-app-components/src/index.js
@@ -7,24 +7,29 @@ import ApprovalCard from './ApprovalCard';
 const App = ()=> {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail 
-        avatar={faker.image.avatar()}
-        author="Sam"
-        timeAgo="Today at 6:00pm"
-        content="Nice blog post!"
-      />
-      <CommentDetail 
-        avatar={faker.image.avatar()}
-        author="Alex"
-        timeAgo="Today at 5:30pm"
-        content="I like the subject!"
-      />
-      <CommentDetail 
-        avatar={faker.image.avatar()}
-        author="Jane"
-        timeAgo="Yesterday at 8:00am"
-        content="I like the writing"/>
+      <ApprovalCard>
+        <CommentDetail 
+          avatar={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 6:00pm"
+          content="Nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          avatar={faker.image.avatar()}
+          author="Alex"
+          timeAgo="Today at 5:30pm"
+          content="I like the subject!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          avatar={faker.image.avatar()}
+          author="Jane"
+          timeAgo="Yesterday at 8:00am"
+          content="I like the writing"/>
+      </ApprovalCard>
     </div>
   );
 }

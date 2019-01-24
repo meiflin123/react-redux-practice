a94133f3afc8e508a0572b9b2211b47a1b4c37ad
Meifang Lin <meiflin123@gmail.com>
2019-01-23 18:03:26 -0800

    passing props from App to CommentDetail

--- mini-app-components/src/index.js
+++ mini-app-components/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
-
+import faker from 'faker';
 
 const App = ()=> {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail 
+        avatar={faker.image.avatar()}
+        author="Sam"
+        timeAgo="Today at 6:00pm"
+        content="Nice blog post!"
+      />
+      <CommentDetail 
+        avatar={faker.image.avatar()}
+        author="Alex"
+        timeAgo="Today at 5:30pm"
+        content="I like the subject!"
+      />
+      <CommentDetail 
+        avatar={faker.image.avatar()}
+        author="Jane"
+        timeAgo="Yesterday at 8:00am"
+        content="I like the writing"/>
     </div>
   );
 }

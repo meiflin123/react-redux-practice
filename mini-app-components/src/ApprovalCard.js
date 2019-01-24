e31939538e5a41984a0fc2d8f560473852dc381b
Meifang Lin <meiflin123@gmail.com>
2019-01-23 18:05:54 -0800

    create and pass ApprovalCard to App

---
+++ mini-app-components/src/ApprovalCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ApprovalCard = () => {
+  return <div>Hi ApprovalCard!</div>
+
+}
+
+
+export default ApprovalCard
--- mini-app-components/src/index.js
+++ mini-app-components/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = ()=> {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail 
         avatar={faker.image.avatar()}
         author="Sam"

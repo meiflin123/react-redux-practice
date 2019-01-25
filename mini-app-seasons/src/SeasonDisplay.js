9887e1977e4e956729d7ef08b37e49f8529f5899
Meifang Lin <meiflin123@gmail.com>
2019-01-24 16:42:45 -0800

    fix indentation

--- mini-app-seasons/src/SeasonDisplay.js
+++ mini-app-seasons/src/SeasonDisplay.js
@@ -9,7 +9,7 @@ const getSeason = (lat, month) => {
 }
 
 const SeasonDisplay = props => {
-	const season = getSeason(props.lat, new Date().getMonth());
+  const season = getSeason(props.lat, new Date().getMonth());
   const text = season === 'winter' ? 'Burr, it is chilly...' : 'Let\'s hit the beach!';
 
   return <div><h1>{text}</h1></div>

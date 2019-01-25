27a9d3e27d566381b60bd0e02b50582866aa0a07
Meifang Lin <meiflin123@gmail.com>
2019-01-24 16:07:31 -0800

    refactor data loading to lifecycle method

--- mini-app-seasons/src/index.js
+++ mini-app-seasons/src/index.js
@@ -4,20 +4,29 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
     
+  }
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude});
-      }, 
-      (err) => {console.log(err)}
+      position => this.setState({ lat: position.coords.latitude }), 
+      err => this.setState({ errorMessage: err.message})
+      
     );
   }
 
-  render() {
-    
-  return <div>Latitude: {this.state.lat} </div>
 
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
   }
 }
 

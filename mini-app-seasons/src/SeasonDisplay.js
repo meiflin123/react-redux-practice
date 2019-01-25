10c4a8294979e4ed8389b40b1c69d314fed67b8a
Meifang Lin <meiflin123@gmail.com>
2019-01-24 16:41:51 -0800

    determine season

--- mini-app-seasons/src/SeasonDisplay.js
+++ mini-app-seasons/src/SeasonDisplay.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const SeasonDisplay = (props) => {
-	console.log(props.lat);
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat < 0 ? 'winter' : 'summer';
+  }
+}
 
-  return <div>Season Display</div>
+const SeasonDisplay = props => {
+	const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === 'winter' ? 'Burr, it is chilly...' : 'Let\'s hit the beach!';
+
+  return <div><h1>{text}</h1></div>
 
 };
 

ecd5386a7c519f4873c7d9fe38c896c3722833c8
Meifang Lin <meiflin123@gmail.com>
2019-01-25 18:50:45 -0800

    get pictures from Unsplash API using axios

--- mini-app-pics/src/components/App.js
+++ mini-app-pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit (term) {
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 
+          'Client-ID 27656bf92ea27bab5cf77c739c662b56adf16de4c86fea61104353d2fc7ea994'
+      }
+    });
 
   }
 

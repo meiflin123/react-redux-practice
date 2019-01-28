66fa7ba5a4a16c22ee9e1c42f3f659fa76b599cf
Meifang Lin <meiflin123@gmail.com>
2019-01-27 19:17:58 -0800

    create component ImageList and render images as user submmit form

--- mini-app-pics/src/components/App.js
+++ mini-app-pics/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
@@ -10,7 +11,6 @@ class App extends React.Component {
       params: { query: term },
      
     });
-    console.log(this)
     this.setState({ images: response.data.results })
 
   }
@@ -18,8 +18,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
-        Found: {this.state.images.length} images
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     )
   }

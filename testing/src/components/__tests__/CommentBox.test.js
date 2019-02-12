8518973dac938c27c6cab8b530ba98e6da2d4c6b
Meifang Lin <meiflin123@gmail.com>
2019-02-12 15:49:43 -0800

    test text area

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -8,7 +8,33 @@ beforeEach(() => {
   wrapped = mount(<CommentBox />);
 });
 
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
-})
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  });
+
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+  });
+
+  it('when form is submitted, text area should be emptied', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update()
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+
+  });
+});
+

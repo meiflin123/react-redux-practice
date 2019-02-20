52093315da9f8babdce59025bdbb7895b725fd47
Meifang Lin <meiflin123@gmail.com>
2019-02-19 22:10:48 -0800

    display posts and corresponding users

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPost }from '../actions'
+import { fetchPosts }from '../actions'
+import UserHeader from './UserHeader';
+
 class PostList extends React.Component {
 
   componentDidMount() {
-    this.props.fetchPost()
+    this.props.fetchPosts()
+  };
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId} />
+          </div>
+        </div>
+      )
+    })
   }
   render() {
     console.log(this.props.posts)
-    return <div>PostList</div>
+    return <div className="ui relaxed divided list">{this.renderList()}</div>
   };
 };
 
@@ -20,4 +39,4 @@ const mapStateToProps = state => {
     
 };
 
-export default connect(mapStateToProps, { fetchPost })(PostList);
+export default connect(mapStateToProps, { fetchPosts })(PostList);
